5f419e72b0629a3d0303acb3b51afef04c0e6b4a
Austin Stokes <austinmprogramming@gmail.com>
2017-05-26 00:23:35 -0700

    added boolean if statement for addition and subtraction

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,34 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
-
+    
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonPressed(_ sender: Any) {
-      theLabel.text = "Answer is: \(Double(text1.text!)! + Double(text2.text!)!)"
+        
+        let addition = false
+        
+        if addition {
+        theLabel.text = "Answer is: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+        theLabel.text = "Answer is: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
+        
     }
     
     @IBAction func noPressMe(_ sender: Any) {
         theLabel.text = "Buttons are pretty cool."
     }
     
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
